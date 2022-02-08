0f624d236013abe2c69a804a3f1157e796350f6f
jayc545 <jayc545htx@gmail.com>
2022-02-08 11:05:39 +0100

    Created a visual studio file

---
+++ Dedicated_Unity_Game_Server/Dedicated_Unity_Game_Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dedicated_Unity_Game_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
